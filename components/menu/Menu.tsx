32ef2798db926aea46a7b3199344d8041f1e00c0
Md Abdul Kader <devabdulemail@gmail.com>
2024-12-01 19:25:03 +0600

    fixed:menu animaiton

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -6,6 +6,7 @@ import { gsap } from "gsap";
 import BurgerMenu from "./BurgerMenu";
 
 const menuLinks = [
+  { path: "/", label: "Md Abdul Kader" },
   { path: "/", label: "Home" },
   { path: "#about", label: "About" },
   { path: "#skills", label: "Skills" },
@@ -39,6 +40,13 @@ const Menu = () => {
           stagger: 0.1,
           ease: "power4.inOut",
           delay: -0.75,
+        })
+        .to(".menu-link", {
+          top: "0rem",
+          duration: 1,
+          stagger: 0.1,
+          ease: "power4.inOut",
+          delay: -0.75,
         });
     }
 
@@ -61,27 +69,26 @@ const Menu = () => {
   }, [isMenuOpen]);
 
   return (
-    <div className="relative border  z-50" ref={container}>
+    <div className="relative  z-50" ref={container}>
       <div className="menu-bar fixed right-20 top-10 z-10  ">
         <div className="menu-open cursor-pointer text-white  burger-menu">
           <BurgerMenu isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
         </div>
       </div>
-      <div className="menu-overlay px-20 w-full relative z-50 pb-10 flex flex-col justify-between overflow-auto">
+      <div
+        className="menu-overlay h-screen w-screen fixed top-0 left-0 bg-[#c5fb45]  z-[1] px-20   pb-10 flex flex-col justify-between overflow-auto"
+        style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)" }}
+      >
         <div
           className="menu-links flex flex-col 
-        pt-40 md:pt-0 w-full"
+      pt-40 md:pt-0 w-full"
         >
           {menuLinks.map((link, index) => (
-            <div
-              key={index}
-              className="w-full" // Ensure full width for each item
-              onClick={toggleMenu}
-            >
-              <div className="menu-link-item-holder py-5 w-full border-b-2 border-black">
+            <div key={index} className="w-full" onClick={toggleMenu}>
+              <div className="menu-link-item-holder py-5 w-full border-b-2 border-black overflow-hidden">
                 <Link
                   href={link.path}
-                  className="menu-link font-bonny-regular text-7xl md:text-[7rem] w-full block text-center" // Add block to make it full width
+                  className="menu-link font-bonny-regular text-7xl md:text-[7rem] w-full block text-center relative top-[8rem] " // Add top value to make it hide
                 >
                   {link.label}
                 </Link>
@@ -89,47 +96,6 @@ const Menu = () => {
             </div>
           ))}
         </div>
-
-        {/* <div className="flex justify-between left-0  py-10 w-full">
-          <div className="flex flex-col">
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Facebook &#8599;
-            </a>
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter &#8599;
-            </a>
-            <a
-              href="https://linkedin.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn &#8599;
-            </a>
-            <a
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Instagram &#8599;
-            </a>
-          </div>
-          <div className="flex flex-col">
-            <p>info@gmail.com</p>
-            <p>3434343</p>
-          </div>
-          <div className="flex flex-col">
-            <p>View Showreel</p>
-            <p>Check Videos</p>
-          </div>
-        </div> */}
       </div>
     </div>
   );
