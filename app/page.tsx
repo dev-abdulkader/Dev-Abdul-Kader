18c053a32f780cbaa9f28eee58b9d7f3b0634158
Md Abdul Kader <devabdulemail@gmail.com>
2026-01-31 11:36:06 +0600

    rem oved lenis

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import Hero from "@/components/home/Hero";
 import ProjectsSection from "@/components/home/project-section";
 import StickyContainer from "@/components/home/sticky-section";
-import Lenis from "@studio-freight/lenis";
 import VideoSection from "@/components/home/video-section";
 import AboutMe from "@/components/home/about-me";
 import Contact from "@/components/home/contact";
